100f4b1d1d383283925310e9c83f59d71f9408d4
jainprachi2506 <jainprachi.55@gmail.com>
2017-08-24 00:28:30 -0700

    find two nodes whose sum is equal to the given target

---
+++ 2017-08-23/find_target.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        stack = []
+        track = []
+        found = False
+        if root is None:
+            return found
+        stack.append(root)
+        while stack:
+            popped = stack.pop()
+            if popped.val not in track:
+                track.append(k-popped.val)
+            else:
+                found = True
+            if popped.left:
+                stack.append(popped.left)
+            if popped.right:
+                stack.append(popped.right)
+                
+        return found
