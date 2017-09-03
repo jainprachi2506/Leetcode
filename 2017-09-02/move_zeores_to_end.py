fe5c2586256987b2d72e7e6f8cf849386014ffff
jainprachi2506 <jainprachi.55@gmail.com>
2017-09-03 14:40:57 -0700

    Easy - Arrays

---
+++ 2017-09-02/move_zeores_to_end.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/move-zeroes/description/
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        i = 0
+        j = 1
+        
+        while j < len(nums):
+            if nums[i] != 0:
+                i += 1
+                j += 1
+            else:
+                if nums[j] != 0:
+                    nums[i] = nums[j]
+                    nums[j] = 0
+                    i += 1
+                    j += 1
+                else:
+                    j += 1
+        
+        
+        
+        
+        
+        
