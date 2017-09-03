b7790d6077fbc1efb67488264b65b574453ab20b
jainprachi2506 <jainprachi.55@gmail.com>
2017-09-03 12:22:57 -0700

    Easy - Arrays

---
+++ 2017-09-02/array_pair_sum.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/array-partition-i/description/
+
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        
+        ret = 0
+        for i in xrange(0, len(nums), 2):
+            ret += nums[i]
+            
+        return ret
+        
