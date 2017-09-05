15b5a6717606eb13b3843d23f4a601fa0a35e7ef
jainprachi2506 <jainprachi.55@gmail.com>
2017-09-04 23:10:17 -0700

    Easy - Dynamic Programming

---
+++ 2017-09-04/reverse_words.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split(' ')
+        
+        for w in xrange(len(words)):
+            word = list(words[w])
+            i = 0
+            j = len(word)-1
+            while i <= j:
+                temp = word[j]
+                word[j] = word[i]
+                word[i] = temp
+                i += 1
+                j -= 1
+            word = ''.join(word)
+            words[w] = word
+        
+        return ' '.join(words)
+            
+                
+            
